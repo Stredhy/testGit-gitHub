d05e5035a862c35af4043931850611060189920b
Stredhy <mimoefect@gmail.com>
2025-04-15 18:37:07 -0600

    Agregado de Scanner

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main{
 	public static void main(String[] args){
+		Scanner cs = new Scanner(System.in);
+		String myStr = new String("Nuevo Commit");
 		System.out.println("Hello World!");	
+		System.out.println(myStr);
 	}
 }
