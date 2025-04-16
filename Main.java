4720311ca652c369bd60eb0ae166d0334bba5a0a
Stredhy <mimoefect@gmail.com>
2025-04-16 14:08:22 -0600

    Agregado de nextLine

--- Main.java
+++ Main.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args){
 		Scanner cs = new Scanner(System.in);
-		String myStr = new String("Nuevo Commit");
-		System.out.println("Hello World!");	
+		String myStr = new String();
+		System.out.println("Rama testGit");
+		myStr = cs.nextLine();	
 		System.out.println(myStr);
+		
 	}
 }
