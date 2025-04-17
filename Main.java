9b1dac4a7360bd7c8e8cc4be6902d35722d35894
Stredhy <mimoefect@gmail.com>
2025-04-16 21:47:47 -0600

    Arreglar main

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner cs = new Scanner(System.in);
+		String myStr = new String();
+		System.out.println("Rama testGit");
+		myStr = cs.nextLine();	
+		System.out.println(myStr);
+	}
+}
